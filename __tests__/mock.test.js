5171a17cae6923e87d281ed5af1c64191a59cfd5
Hunter Danielson <hunterleedanielson@gmail.com>
2020-06-17 16:35:26 -0700

    added mock

---
+++ __tests__/mock.test.js
@@ -0,0 +1,24 @@
+const { getCharacter } = require('../lib/getCharacter');
+
+jest.mock('superagent', () => ({
+  get: () => {
+    return Promise.resolve({
+      body: {
+        id: 1,
+        name: 'Rick Sanchez',
+        status: 'Alive',
+        species: 'Human'
+      }
+      
+    });
+  }
+}));
+
+describe('rick and morty api functions', () => {
+  it('can get by character id', () => {
+    return getCharacter(1)
+      .then(response => {
+        expect(response).toEqual({ 'name': 'Rick Sanchez', 'species': 'Human', 'status': 'Alive' });
+      });
+  });
+});
